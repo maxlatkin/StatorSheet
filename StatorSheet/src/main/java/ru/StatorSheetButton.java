de521574d9d0f42d161af4933725194779f5f822
11szerzxr <11szerzxr@10.10.25.26>
2020-04-20 10:57:08 +0300

    using Logback

--- StatorSheet/src/main/java/ru/StatorSheetButton.java
+++ StatorSheet/src/main/java/ru/StatorSheetButton.java
@@ -1,9 +1,10 @@
 package ru;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import java.io.InputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.ptc.cipjava.jxthrowable;
 import com.ptc.pfc.pfcCommand.UICommand;
@@ -11,21 +12,21 @@ import com.ptc.pfc.pfcSession.CreoCompatibility;
 import com.ptc.pfc.pfcSession.Session;
 import com.ptc.pfc.pfcSession.pfcSession;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+
 public class StatorSheetButton {
 	private static final String MSG_FILE = "stator_sheet_button.txt";
-	public static final Logger LOG = Logger.getLogger(StatorSheetButtonListener.class.getName());
+	public static final Logger LOG = LoggerFactory.getLogger(StatorSheetButton.class);
 	
 	private StatorSheetButton() {
     throw new IllegalStateException("Utility class");
   }
 
 
-    public static void start() throws jxthrowable{ 
-		try {
-			LogManager.getLogManager().readConfiguration(StatorSheetButtonListener.class.getResourceAsStream("/logging.properties"));
-		} catch (IOException e) {
-		    System.err.println("Could not setup logger configuration: " + e.toString());
-		}
+    public static void start() throws jxthrowable{
+    	setLogbackFile();
 		LOG.info("The StatorSheet application has begun its work.");
     	
     	Session session = pfcSession.GetCurrentSessionWithCompatibility(CreoCompatibility.C4Compatible);			 
@@ -35,6 +36,20 @@ public class StatorSheetButton {
     }
 
     public static void stop() throws jxthrowable{
-    	LOG.log(Level.INFO, "The StatorSheet application has completed its work.");
+    	LOG.info("The StatorSheet application has completed its work.");
+    }
+    
+    private static void setLogbackFile() {
+    	try {
+			LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+			context.reset();
+			JoranConfigurator configurator = new JoranConfigurator();
+			InputStream stream = StatorSheetButton.class.getResourceAsStream("/logback.xml");
+			configurator.setContext(context);
+			configurator.doConfigure(stream);
+			stream.close();
+		} catch (JoranException | IOException e) {
+			e.printStackTrace();
+		}
     }
 }
