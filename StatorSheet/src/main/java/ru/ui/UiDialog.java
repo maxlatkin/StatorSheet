a637ad8f40104fc5050b5cd5f9223f7f0b0a2104
11szerzxr <11szerzxr@10.10.25.118>
2020-06-18 10:06:04 +0300

    added numbers filter

--- StatorSheet/src/main/java/ru/ui/UiDialog.java
+++ StatorSheet/src/main/java/ru/ui/UiDialog.java
@@ -8,11 +8,8 @@ import com.ptc.cipjava.stringseq;
 import com.ptc.pfc.pfcCommand.DefaultUICommandActionListener;
 import com.ptc.uifc.uifcCheckButton.uifcCheckButton;
 import com.ptc.uifc.uifcComponent.uifcComponent;
-import com.ptc.uifc.uifcInputPanel.uifcInputPanel;
 import com.ptc.uifc.uifcLabel.uifcLabel;
 import com.ptc.uifc.uifcOptionMenu.uifcOptionMenu;
-import com.ptc.uifc.uifcPushButton.PushButton;
-import com.ptc.uifc.uifcPushButton.PushButtonListener_u;
 import com.ptc.uifc.uifcPushButton.uifcPushButton;
 
 import ru.wnc.documents.DocNumbers;
@@ -53,10 +50,9 @@ public class UiDialog extends DefaultUICommandActionListener {
 			uifcCheckButton.CheckButtonFind(DIALOG, SCREW_4_CB).AddActionListener(new UiFourthScrewCBListener());
 			
 			
-//			setNumbersToOM("","OM_Note", DocumentTypes.CALC_AND_WIND_NOTE);
-			setNumbersToOM("","OM_STO", DocumentTypes.STO);
-			setNumbersToOM("","OM_Results", DocumentTypes.MECH_CALC_RESULTS);
-			filterNoteNumber();
+			uifcPushButton.PushButtonFind(DIALOG, "PB_Note").AddActionListener(new UiNotePbListener());
+			uifcPushButton.PushButtonFind(DIALOG, "PB_STO").AddActionListener(new UiStoPbListener());
+			uifcPushButton.PushButtonFind(DIALOG, "PB_Results").AddActionListener(new UiResultsPbListener());
 			
 			uifcLabel.LabelFind(DIALOG, "L_S1_Image").SetImage("Screw\\s1.png");
 			uifcLabel.LabelFind(DIALOG, "L_S2_Image").SetImage("Screw\\s2.png");
@@ -73,97 +69,7 @@ public class UiDialog extends DefaultUICommandActionListener {
 		}
 	}
 
-	private void filterNoteNumber() throws jxthrowable {
-		uifcPushButton.PushButtonFind(DIALOG, "PB_Note").AddActionListener(new PushButtonListener_u() {
-			
-			@Override
-			public void OnMouseExit(PushButton arg0) throws jxthrowable {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void OnMouseEnter(PushButton arg0) throws jxthrowable {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void OnHelp(PushButton arg0) throws jxthrowable {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void OnFocusOut(PushButton arg0) throws jxthrowable {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void OnFocusIn(PushButton arg0) throws jxthrowable {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void OnEndDrag(PushButton arg0) throws jxthrowable {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void OnDropNowhere(PushButton arg0) throws jxthrowable {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void OnDrop(PushButton arg0) throws jxthrowable {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void OnDragNowhere(PushButton arg0) throws jxthrowable {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void OnDragMove(PushButton arg0) throws jxthrowable {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void OnDragExit(PushButton arg0) throws jxthrowable {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void OnDragEnter(PushButton arg0) throws jxthrowable {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void OnBeginDrag(PushButton arg0) throws jxthrowable {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void OnActivate(PushButton arg0) throws jxthrowable {
-				// TODO Auto-generated method stub
-				String noteNumberFilter = uifcInputPanel.InputPanelFind(DIALOG, "IP_Note").GetStringValue();
-				setNumbersToOM(noteNumberFilter,"OM_Note", DocumentTypes.CALC_AND_WIND_NOTE);
-			}
-		});
-	}
-
-	private void setNumbersToOM(String numberFilter,String omName, DocumentTypes type) throws jxthrowable {
+	public void setNumbersToOM(String numberFilter,String omName, DocumentTypes type) throws jxthrowable {
 		stringseq numbers = stringseq.create();
 		DocNumbers docNumbers = new DocNumbers(type);
 		for (String number: docNumbers.getSetOfDocNumbers(numberFilter)) {
---
+++ StatorSheet/src/main/java/ru/ui/UiResultsPbListener.java
@@ -0,0 +1,97 @@
+package ru.ui;
+
+import com.ptc.cipjava.jxthrowable;
+import com.ptc.uifc.uifcInputPanel.uifcInputPanel;
+import com.ptc.uifc.uifcPushButton.PushButton;
+import com.ptc.uifc.uifcPushButton.PushButtonListener_u;
+
+import ru.wnc.documents.DocumentTypes;
+
+public class UiResultsPbListener extends PushButtonListener_u {
+
+	@Override
+	public void OnActivate(PushButton arg0) throws jxthrowable {
+		// TODO Auto-generated method stub
+		String resultsNumberFilter = uifcInputPanel.InputPanelFind(UiDialog.DIALOG, "IP_Results").GetStringValue();
+		UiDialog.getInstance().setNumbersToOM(resultsNumberFilter,"OM_Results", DocumentTypes.MECH_CALC_RESULTS);
+	}
+
+	@Override
+	public void OnBeginDrag(PushButton arg0) throws jxthrowable {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnDragEnter(PushButton arg0) throws jxthrowable {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnDragExit(PushButton arg0) throws jxthrowable {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnDragMove(PushButton arg0) throws jxthrowable {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnDragNowhere(PushButton arg0) throws jxthrowable {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnDrop(PushButton arg0) throws jxthrowable {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnDropNowhere(PushButton arg0) throws jxthrowable {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnEndDrag(PushButton arg0) throws jxthrowable {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnFocusIn(PushButton arg0) throws jxthrowable {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnFocusOut(PushButton arg0) throws jxthrowable {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnHelp(PushButton arg0) throws jxthrowable {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnMouseEnter(PushButton arg0) throws jxthrowable {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnMouseExit(PushButton arg0) throws jxthrowable {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
